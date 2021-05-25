d44517b0a7cce662da7c60a154e8ec69853e5bea
jayber-dev <79035534+jayber-dev@users.noreply.github.com>
2021-05-25 21:27:52 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+ALPHAVANTAGE_API_KEY = "UM9TMTRB3BWDKANL"
+
+ALPHAVANTAGE_API_PARAMS = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": "TSLA",
+    "interval": "60min",
+    "apikey": "UM9TMTRB3BWDKANL"
+}
+import requests
+import os
+
+stock_price = requests.get("https://www.alphavantage.co/query", params=ALPHAVANTAGE_API_PARAMS)
+
+print(stock_price.text)
+## STEP 1: Use https://newsapi.org/docs/endpoints/everything
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+# HINT 1: Get the closing price for yesterday and the day before yesterday. Find the positive difference between the two prices. e.g. 40 - 20 = -20, but the positive difference is 20.
+# HINT 2: Work out the value of 5% of yerstday's closing stock price.
+
+
+## STEP 2: Use https://newsapi.org/docs/endpoints/everything
+# Instead of printing ("Get News"), actually fetch the first 3 articles for the COMPANY_NAME. 
+# HINT 1: Think about using the Python Slice Operator
+
+
+## STEP 3: Use twilio.com/docs/sms/quickstart/python
+# Send a separate message with each article's title and description to your phone number. 
+# HINT 1: Consider using a List Comprehension.
+
+
+# Optional: Format the SMS message like this:
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
